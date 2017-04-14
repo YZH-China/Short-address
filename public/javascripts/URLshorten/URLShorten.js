8881692d7a5043614ff4c27acbb6d6129afbbc4a
YZH-cd <289081808@qq.com>
2017-04-14 09:20:09 +0800

    短地址demo

---
+++ public/javascripts/URLshorten/URLShorten.js
@@ -0,0 +1,50 @@
+var crypto = require('crypto');
+
+exports.urlShorten = {
+
+	text: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', /*组成短URL的字符*/
+
+	/**
+	 * [md5 产生长URL的MD5码]
+	 * @param  {[string]} longURL [长URL本身]
+	 * @return {[int]}         [128位的MD5码]
+	 */
+	md5: function(longURL){
+		var md5num = crypto.createHash('md5'); /*产生hash对象，用于生成MD5码*/
+		md5num.update(longURL, 'utf8'); //生成md5码
+		var str = md5num.digest('hex'); //转换为16进制数字
+		return str;
+	},
+
+	/**
+	 * [getShort 获取短地址]
+	 * @param  {[string]} longURL [长地址]
+	 * @return {[string]}         [短地址]
+	 */
+	getShort: function(longURL){
+		var shortArr = [];
+		var md5str = this.md5(longURL);
+		
+		for(var i = 0; i < 4; i += 1){
+			var curStr = md5str.slice(i * 8, i * 8 + 8);
+			var shorturl = '';
+
+			curStr = parseInt(curStr, 16);
+			curStr &= 0x3FFFFFFF; /*取低30位*/
+
+			for(var j = 0; j < 6; j += 1){
+				shorturl += (this.text[curStr & 0x0000003D]);
+				curStr >>= 5;
+			}
+
+			shortArr.push(shorturl);
+		}
+		return shortArr[(Math.floor(Math.random() * 4))];
+	}
+
+}
+
+// 1.将原始长链接进行MD5加密，为了避免防止算法泄漏，可以在原链接上添加自定义的字符串作为密钥。
+// 2.把128位的MD分成四组，每组32位，对应一个候选短链接。
+// 3.对于每个32位的数，将它与0x3FFFFFFF进行位与运算，取其低30位的数据。把得到的值与0x0000003D进行位与运算，再把得到的结果作为下标在字符表中选取字符，再把原数字右移5位进行相同操作，重复进行6次得到6个字符，即组成一个候选短链接地址。
+// 4.在4个候选短链接中随机选择一个作为最终的短链接，把长短链接映射关系存入数据库中。
---
+++ public/javascripts/URLshorten/main.js
@@ -0,0 +1,3 @@
+var URLShorten = require('./URLShorten.js');
+
+console.log(URLShorten.urlShorten.getShort('http://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=02049043_69_pg&wd=nodejs%20md5%E7%AE%97%E6%B3%95&oq=nodejs%2520md5%25E7%25AE%2597%25E6%25B3%2595%25E9%2582%25A3&rsv_pq=fd438521000a7ba8&rsv_t=d6f60uBSJwmATgswvLCLtu7m%2BoNR%2FCzXBOU7UjFvsU3ylnCDsuz83kbTj7qIpCGPjgKx3SQ&rqlang=cn&rsv_enter=0&rsv_sug3=5&rsv_sug1=1&rsv_sug7=100&inputT=4484&rsv_sug4=4984'));
